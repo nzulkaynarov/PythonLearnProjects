52a6e04547b4834cfc31ca508cb3e7a0d5d7001d
Nurbek <nurbek@zulkaynarov.com>
2025-03-26 12:23:05 +0500

    Изменена логика обработки времени занятий спортом: добавлено ограничение на 45 минут и вывод сообщения о завершении тренировки.

--- Skillbox/Chapter 1/part 7/task 7.4.3.py
+++ Skillbox/Chapter 1/part 7/task 7.4.3.py
@@ -4,8 +4,9 @@ count_work = 0
 
 for time in range(start_work, 20):
 	sport_time = int(input("Сколько минут вы занимались спортом: "))
-	if action_time <= 45:
-		break
-	action_time += sport_time
 	count_work += 1
-print("Вы работали", count_work, "часа", "и занимались спортом", action_time, "минут")
+	action_time += sport_time
+	if action_time > 45:
+		print("На сегодня спорта хватит")
+		break
+print("Вы работали", count_work, "ч.", "и занимались спортом", action_time, "минут")
