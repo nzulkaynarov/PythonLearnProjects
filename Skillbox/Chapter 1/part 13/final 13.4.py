d845cf0ba6c0e31e0b2005947b38540d99b2cd5e
Nurbek <nurbek@zulkaynarov.com>
2025-04-17 16:40:16 +0500

    Рефакторинг кода: вынесены функции для подсчета цифр и изменения числа в отдельные функции. Упрощена логика обработки ввода и вывода результатов.

--- Skillbox/Chapter 1/part 13/final 13.4.py
+++ Skillbox/Chapter 1/part 13/final 13.4.py
@@ -1,35 +1,30 @@
-first_n = int(input("Введите первое число: "))
-first_num_count = 0
-temp = first_n
+def count_numbers(number):
+	num_count = 0
+	temp = number
+	while temp > 0:
+		num_count += 1
+		temp = temp // 10
+	return num_count
 
+def change_number(number):
+    digits_count = count_numbers(number)
+    last_digit = number % 10
+    first_digit = number // 10 ** (digits_count - 1)
+    between_digits = number % 10 ** (digits_count - 1) // 10
+    new_number = last_digit * 10 ** (digits_count - 1) + between_digits * 10 + first_digit
+    return new_number
 
-while temp > 0:
-    first_num_count += 1
-    temp = temp // 10
+def main():
+	first_n = int(input("Введите первое число: "))
+	if count_numbers(first_n) < 3:
+		print("В первом числе меньше трёх цифр.")
+	else:
+		print('Изменённое первое число:', change_number(first_n))
+	second_n = int(input("\nВведите второе число: "))
+	if count_numbers(second_n) < 4:
+		print("Во втором числе меньше четырёх цифр.")
+	else:
+		print('Изменённое второе число:', change_number(second_n))
+		print('\nСумма чисел:', first_n + second_n)
 
-
-if first_num_count < 3:
-    print("В первом числе меньше трёх цифр.")
-else:
-    last_digit = first_n % 10
-    first_digit = first_n // 10 ** (first_num_count - 1)
-    between_digits = first_n % 10 ** (first_num_count - 1) // 10
-    first_n = last_digit * 10 ** (first_num_count - 1) + between_digits * 10 + first_digit
-    print('Изменённое первое число:', first_n)
-    second_n = int(input("\nВведите второе число: "))
-    second_num_count = 0
-    temp = second_n
-
-
-    while temp > 0:
-        second_num_count += 1
-        temp = temp // 10
-    if second_num_count < 4:
-        print("Во втором числе меньше четырёх цифр.")
-    else:
-        last_digit = second_n % 10
-        first_digit = second_n // 10 ** (second_num_count - 1)
-        between_digits = second_n % 10 ** (second_num_count - 1) // 10
-        second_n = last_digit * 10 ** (second_num_count - 1) + between_digits * 10 + first_digit
-        print('Изменённое второе число:', second_n)
-        print('\nСумма чисел:', first_n + second_n)
+main()
